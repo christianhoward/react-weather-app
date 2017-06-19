b33cb186c4ceba2ab61fbab620977b1a6a21dae5
christianhoward <christian.john.howard@gmail.com>
2017-06-19 08:59:41 -0700

    Initial Commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=7ae8db2003e5cf0ff60bdf9a095206d3&units=imperial';
+
+module.exports = {
+  getTemp: function(location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    return axios.get(requestUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ app/components/About.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+var About = (props) => {
+  return (
+    <h3>About component</h3>
+  )
+};
+
+module.exports = About;
