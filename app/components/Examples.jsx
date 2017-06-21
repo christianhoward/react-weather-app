257278ea973ba1b45762a5e40be7219c9e8c3bf4
christianhoward <christian.john.howard@gmail.com>
2017-06-21 15:48:40 -0400

    Added Foundation CSS to app for styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 const React = require('react');
+const {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=arlington'>Arlington, VA</Link>
+        </li>
+        <li>
+          <Link to='/?location=seattle'>Seattle, WA</Link>
+        </li>
+      </ol>
     </div>
   )
 };
